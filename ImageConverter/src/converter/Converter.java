28afbd243960962d4b1a7112a667149633cf7ed7
Hasan VATANSEVER <hasanvatansever@outlook.com>
2015-03-19 23:41:24 +0200

    Image Converter
    "Image to base64" and "base64 to Image" converter for image portability.

---
+++ ImageConverter/src/converter/Converter.java
@@ -0,0 +1,48 @@
+package converter;
+ 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.apache.commons.codec.binary.Base64;
+ 
+public class Converter {
+
+    public static void main(String[] args) {
+       
+        File file = new File("Image\\Image1.png");
+        
+        try {
+            FileInputStream image = new FileInputStream(file);
+            byte imageData[] = new byte[(int) file.length()];
+            image.read(imageData);
+ 
+            String imageString = encodeImage(imageData);
+            writeToTxt(imageString);
+            System.out.println(imageString);
+        
+            System.out.println(" Successfull");
+         
+        } catch (IOException e) {
+            System.out.println( e);
+        }
+    }
+    
+    public static String encodeImage(byte[] imageByteArray) {
+        return Base64.encodeBase64URLSafeString(imageByteArray);
+    }
+    
+    public static  void writeToTxt(String x64Image) {
+
+        try{
+            FileWriter outFile = new FileWriter(new File("Result\\x64Image.txt"));
+            PrintWriter pWriter = new PrintWriter(outFile);
+            pWriter.print(x64Image);
+            pWriter.close();
+                
+            }catch (Exception e){
+                System.err.println(e);
+            }
+    }
+}
